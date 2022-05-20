308f7dd7458199f13d84bbeda1d337bb38e91116
sjhu2020 <sjhu@uw.edu>
2022-05-19 21:36:14 -0700

    Update aggregate.r

---
+++ aggregate.r
@@ -0,0 +1,9 @@
+library("dplyr")
+
+games_df <- read.csv("data/Games.csv")
+
+genres_df <- select(games_df, Genre)
+
+genres_grouped_df <- group_by(genres_df, Genre)
+
+genres_grouped_freq_df <- summarize(genres_grouped_df, Total = length(Genre))
