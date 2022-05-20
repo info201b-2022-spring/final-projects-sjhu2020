fb82833c3b777c91d2332116e429b8d563e9bb45
vinliu20 <vinliu20@uw.edu>
2022-05-19 18:58:53 -0700

    forgot to save

--- chart_3.r
+++ chart_3.r
@@ -4,11 +4,16 @@ library(reshape2)
 
 file3 <- read_csv(file = "data/multiTimeline.csv")
 melted_df <- melt(file3, id = "dates")
-dates <- as.Date(file3$dates)
-date_months <- month(dates)
-processed_df <- data.frame(dates ,melted_df)
-new_df <- left_join(file3, processed_df) 
 
 genre_trend_line <- ggplot(melted_df, aes(x = dates, y = value, color = variable)) + 
-  geom_line() 
+  geom_line() +
+  labs(
+    title = "Video Game Search Popularity by Genre",
+    subtitle = "",
+    caption = "Source: Google Trends",
+    x = "Date From 2019 ~ Now, Collected Once A Week",
+    y = "Number of Searches"
+  )
+
+genre_trend_line
 
