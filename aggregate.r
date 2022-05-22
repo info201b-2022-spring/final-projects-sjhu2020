081e686157de00daed8572d79941acf9350d7a70
Vinson Liu <vinliu20@uw.edu>
2022-05-22 15:17:02 -0700

    cant get kable to work

--- aggregate.r
+++ aggregate.r
@@ -1,9 +1,14 @@
 library("dplyr")
+library(knitr)
 
-games_df <- read.csv("data/Games.csv")
+games_df <- read.csv("data/Games.csv", encoding = "UTF-8")
 
 genres_df <- select(games_df, Genre)
 
 genres_grouped_df <- group_by(genres_df, Genre)
 
 genres_grouped_freq_df <- summarize(genres_grouped_df, Total = length(Genre))
+
+table <- kable(genres_grouped_freq_df)
+
+table
--- chart_1.r
+++ chart_1.r
@@ -1,7 +1,7 @@
 library("dplyr")
 library("wordcloud")
 
-games_df <- read.csv("data/Games.csv")
+games_df <- read.csv("data/Games.csv", encoding = "UTF-8")
 
 genres_df <- select(games_df, Genre)
 
@@ -9,6 +9,6 @@ genres_grouped_df <- group_by(genres_df, Genre)
 
 genres_grouped_freq_df <- summarize(genres_grouped_df, Total = length(Genre))
 
-wordcloud(genres_grouped_freq_df$Genre , genres_grouped_freq_df$Total ,  alpha=0.9 , rot.per=0.3 )
+cloud <- wordcloud(genres_grouped_freq_df$Genre , genres_grouped_freq_df$Total ,  alpha=0.9 , rot.per=0.3 )
 # create word cloud displaying frequency of video game genres
 
