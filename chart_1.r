49c74b6640e6f6c8a195d121b9a1f9f48e5c036d
sjhu2020 <sjhu@uw.edu>
2022-05-22 14:13:21 -0700

    fix chart_1 error

--- chart_1.r
+++ chart_1.r
@@ -9,6 +9,6 @@ genres_grouped_df <- group_by(genres_df, Genre)
 
 genres_grouped_freq_df <- summarize(genres_grouped_df, Total = length(Genre))
 
-wordcloud(genres_grouped_freq_df$Genre , genres_grouped_freq_df$Total , col=terrain.colors(length(genre_list) , alpha=0.9) , rot.per=0.3 )
+wordcloud(genres_grouped_freq_df$Genre , genres_grouped_freq_df$Total ,  alpha=0.9 , rot.per=0.3 )
 # create word cloud displaying frequency of video game genres
 
