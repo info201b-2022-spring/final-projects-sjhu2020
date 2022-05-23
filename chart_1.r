696b9309b295abb5c0308a2066012b684db6397e
sjhu2020 <sjhu@uw.edu>
2022-05-22 22:29:47 -0700

    Update chart_1.r

--- chart_1.r
+++ chart_1.r
@@ -12,3 +12,4 @@ genres_grouped_freq_df <- summarize(genres_grouped_df, Total = length(Genre))
 cloud <- wordcloud(genres_grouped_freq_df$Genre , genres_grouped_freq_df$Total ,  alpha=0.9 , rot.per=0.3 )
 # create word cloud displaying frequency of video game genres
 
+
